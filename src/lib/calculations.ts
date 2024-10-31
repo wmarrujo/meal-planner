fc7e3bc8c770eab5fdd5f3573fdcd36263bd9690
William Marrujo <willmarrujo@gmail.com>
2024-10-30 23:52:38 -0500

    start calculation

---
+++ src/lib/calculations.ts
@@ -0,0 +1,34 @@
+import {solve} from "yalps"
+
+////////////////////////////////////////////////////////////////////////////////
+
+function targetCalories(age: number, sex: number, height: number, weight: number, goal: number, activity: number): number {
+	const sexAdjustment = 166 * sex - 161
+	const restingEnergyExpenditureCalories = weight * 10 + height * 6.25 - age * 5 + sexAdjustment
+	const activityMultiplier = 0.175 * activity + 0.85
+	const totalEnergyExpenditureCalories = restingEnergyExpenditureCalories * activityMultiplier
+	const goalMultiplier = 0.15 * goal + 0.55
+	return totalEnergyExpenditureCalories * goalMultiplier
+}
+
+function targetProtein(weight: number, activity: number): number {
+	return weight * (-1/3 * activity**2 + 13/30 * activity + 0.8)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function model() {
+	return {
+		direction: "maximize" as const,
+		objective: "profit",
+		constraints: {
+			wood: {max: 300},
+			labor: {max: 110},
+			storage: {max: 400},
+		},
+		variables: {
+			table: {wood: 30, labor: 5, profit: 1200, storage: 30},
+			dresser: {wood: 20, labor: 10, profit: 1600, storage: 50},
+		},
+	}
+}
